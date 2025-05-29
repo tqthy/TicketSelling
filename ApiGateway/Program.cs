f138b210feaa7de4c5c2ea2b821ec519a32adeef
Thy Tran <22521461@gm.uit.edu.vn>
2025-05-29 23:47:31 +0700

    feat: add CORS configuration to allow any origin, method, and header

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -43,6 +43,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -51,6 +61,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseForwardedHeaders();
 // app.UseHttpsRedirection();
